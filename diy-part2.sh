c22775ed928e74f56aed6b17a915cbf23da870f2
NothingNA <105407073+NothingNA@users.noreply.github.com>
2025-07-31 14:16:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,10 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
+rm -rf feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # Modify default IP
 sed -i 's/10.10.10.10/10.10.50.50/g' package/base-files/files/bin/config_generate
 
