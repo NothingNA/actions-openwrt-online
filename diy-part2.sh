1ebf2bf082741c54f6a51d6ebf9ecff372a2f52e
NothingNA <105407073+NothingNA@users.noreply.github.com>
2025-07-31 14:11:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/10.10.10.10/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.10/10.10.50.50/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
